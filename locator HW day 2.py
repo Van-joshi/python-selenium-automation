72ab4e5b0f3d6a22eee14953d24440fe8c57bfaa
Your Name <you@example.com>
2023-05-01 20:45:08 -0400

    updated locator HW day 2

---
+++ locator HW day 2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.amazon.com/')
+# Amacon logo
+driver.find_element(By.XPATH, "//a[@id='nav-logo-sprites']")
+# Email Field
+# driver.find_element(By.XPATH, "//a[@id='nav-link-accountList']").click()
+# driver.find_element(By.ID, "ap_email").send_keys("vjtest@gmail.com")
+# driver.find_element(By.ID, "continue").click()
+# driver.find_element(By.XPATH, "(//span[@class='a-expander-prompt'])[1]")
+# driver.find_element(By.ID, "ap-other-signin-issues-link")
+# driver.find_element(By.ID, "createAccountSubmit")
+#driver.find_element(By.XPATH, "//a[@class='a-link-normal'][normalize-space()='Conditions of Use']")
+#driver.find_element(By.XPATH, "//a[@class='a-link-normal'][normalize-space()='Privacy Notice']")
+driver.find_element(By.XPATH, "//span[normalize-space()='& Orders']").click()
+expected_result= 'Mobile phone number or email'
+actual_result=driver.find_element(By.XPATH, "//label[normalize-space()='Mobile phone number or email']").text
+if expected_result == actual_result:
+    print("Test Passed")
+expected_result1= 'Sign in'
+actual_result1=driver.find_element(By.CSS_SELECTOR, "h1[class='a-spacing-small']").text
+if expected_result1 == actual_result1:
+    print("Test 1 passed")
