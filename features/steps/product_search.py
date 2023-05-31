f0ddff7c70310909bc5db5c7c3377b8599687543
Vandana J <Vandana.joshi@gmail.com>
2023-05-31 18:52:02 -0400

    product search whole page

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -7,26 +7,33 @@ SEARCH_INPUT = (By.NAME, 'q')
 SEARCH_SUBMIT = (By.NAME, 'btnK')
 
 
-@given('Open Google page')
+@given('Open Amazon page')
 def open_google(context):
-    context.driver.get('https://www.google.com/')
+    context.driver.get('https://www.Amazon.com/')
 
 
 @when('Input {search_word} into search field')
 def input_search(context, search_word):
-    search = context.driver.find_element(*SEARCH_INPUT)
-    search.clear()
-    search.send_keys(search_word)
+    context.driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('shoes')
+    context.driver.find_element(By.ID, 'nav-search-submit-button').click()
     sleep(4)
 
 
 @when('Click on search icon')
 def click_search_icon(context):
-    context.driver.find_element(*SEARCH_SUBMIT).click()
-    sleep(1)
+    context.driver.find_element(By.ID, 'nav-search-submit-button').click()
+sleep(1)
 
 
-@then('Product results for {search_word} are shown')
-def verify_found_results_text(context, search_word):
-    assert search_word.lower() in context.driver.current_url.lower(), \
-        f'Expected query not in {context.driver.current_url.lower()}'
+@then('Product results for shoes are shown')
+def verify_found_results_text(context):
+    all_products=context.driver.find_elements(By.CSS_SELECTOR, '[data-component-type="s-search-result"]')
+    #print(all_products)
+    for product in all_products:
+        title=product.find_element(By.XPATH, "(//span[@class='a-size-base-plus a-color-base'][normalize-space()='Amazon Essentials'])[1]").text
+        print(title)
+
+
+
+
+
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -1,7 +1,7 @@
 Feature: Test Scenarios for Search functionality
 
   Scenario: User can search for a product
-    Given Open Google page
-    When Input Car into search field
+    Given Open Amazon page
+    When Input shoes into search field
     And Click on search icon
-    Then Product results for Car are shown
+    Then Product results for shoes are shown
