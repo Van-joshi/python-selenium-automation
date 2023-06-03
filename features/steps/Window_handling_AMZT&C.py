e4a2ad66fcd554b6720258b0a39b27499a7a6d0e
Vandana J <Vandana.joshi@gmail.com>
2023-06-03 13:07:09 -0400

    modified window handle

--- features/steps/Window_handling_AMZT&C.py
+++ features/steps/Window_handling_AMZT&C.py
@@ -7,31 +7,31 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 privacy_link=(By.XPATH, "//a[@class='a-link-normal'][normalize-space()='Privacy Notice']")
 @then ('Store Original window')
-def Store_original_window(context):
+def store_original_window(context):
     context.original_window=context.driver.current_window_handle
     print("Original:", context.original_window )
     print("All Windows:", context.driver.window_handles )
 @then('Click on Privacy Notice')
-def Click_On_PN(context):
+def click_on_pn(context):
     context.driver.find_element(*privacy_link).click()
     print("All windows:", context.driver.window_handles)
 
 @then('Switch to new window')
-def Switch_to_new_window(context):
+def switch_to_new_window(context):
     context.driver.wait.until(EC.new_window_is_opened)
     all_windows=context.driver.window_handles
     print("After all windows are open-All windows:", all_windows)
     context.driver.switch_to.window(all_windows[1])
 @then( 'TC Page is Open')
-def TCPage_is_open(context):
+def tc_page_is_open(context):
     context.driver.wait.until(EC.url_contains('https://www.amazon.com/gp/help/customer/display'))
 
-@then ('Close TC page')
-def Close_TCpage(context):
+@then('Close TC page')
+def close_tc_page(context):
     context.driver.close()
 
-@then ('Return to original window')
-def Return_toOrg_window(context):
+@then('Return to original window')
+def return_to_org_window(context):
     all_windows = context.driver.window_handles
     context.driver.switch_to.window(context.original_window)
     print("original", context.original_window)
